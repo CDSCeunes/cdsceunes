d71eef3e6415c43c74b0183976c5c79804c81911
Nikolas <nikolas.serafini@gmail.com>
2015-08-24 08:57:19 -0300

    Fix de erros de compilação

--- src/main/java/br/ufes/cdsceunes/controller/DepartmentController.java
+++ src/main/java/br/ufes/cdsceunes/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package br.ufes.cdsceunes.controller;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
