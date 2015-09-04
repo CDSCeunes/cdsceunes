ca21dd8dcf77aa6c0d7f5b4ad46a8cd7614a8383
nikolas <nikolas.serafini@gmail.com>
2015-09-04 10:27:14 -0300

    Controllers para disciplina e turma criados.

---
+++ src/main/java/br/ufes/cdsceunes/controller/DisciplineController.java
@@ -0,0 +1,49 @@
+package br.ufes.cdsceunes.controller;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.ufes.cdsceunes.dao.DisciplineDAO;
+import br.ufes.cdsceunes.model.Discipline;
+
+@Controller
+@Transactional
+@RequestMapping("/disciplines")
+public class DisciplineController extends AbstractController {
+	
+	@Autowired
+	private DisciplineDAO disciplines;
+	
+	@RequestMapping("/")
+	public ModelAndView list() {
+		ModelAndView mad = new ModelAndView("disciplines/list");
+		mad.addObject("departments",disciplines.list());
+		return mad;
+	}
+
+	@RequestMapping(value = "/form", name = "addDiscipline")
+	public ModelAndView form(@ModelAttribute Discipline discipline) {
+		ModelAndView mad = new ModelAndView("disciplines/form");
+		return mad;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, name="createDiscipline", value="save")
+	public ModelAndView save(@ModelAttribute("discipline") @Valid Discipline discipline, BindingResult binding,
+			RedirectAttributes redirectAttributes) {
+		if (binding.hasErrors()) {
+			return form(discipline);
+		}
+		disciplines.save(discipline);
+		redirectAttributes.addFlashAttribute("sucess", "Disciplina cadastrado com sucesso!");
+		return new ModelAndView("redirect:");
+	}
+}
---
+++ src/main/java/br/ufes/cdsceunes/controller/OfferedClassController.java
@@ -0,0 +1,49 @@
+package br.ufes.cdsceunes.controller;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.ufes.cdsceunes.dao.ClassDAO;
+import br.ufes.cdsceunes.model.OfferedClass;
+
+@Controller
+@Transactional
+@RequestMapping("/classes")
+public class OfferedClassController extends AbstractController{
+
+	@Autowired
+	private ClassDAO classes;
+	
+	@RequestMapping("/")
+	public ModelAndView list() {
+		ModelAndView mad = new ModelAndView("classes/list");
+		mad.addObject("classes",classes.list());
+		return mad;
+	}
+
+	@RequestMapping(value = "/form", name = "addClasses")
+	public ModelAndView form(@ModelAttribute OfferedClass offeredClass) {
+		ModelAndView mad = new ModelAndView("classes/form");
+		return mad;
+	}
+
+	@RequestMapping(method = RequestMethod.POST, name="createClasses", value="save")
+	public ModelAndView save(@ModelAttribute("offeredClass") @Valid OfferedClass offeredClass, BindingResult binding,
+			RedirectAttributes redirectAttributes) {
+		if (binding.hasErrors()) {
+			return form(offeredClass);
+		}
+		classes.save(offeredClass);
+		redirectAttributes.addFlashAttribute("sucess", "Turma cadastrada com sucesso!");
+		return new ModelAndView("redirect:");
+	}
+}
