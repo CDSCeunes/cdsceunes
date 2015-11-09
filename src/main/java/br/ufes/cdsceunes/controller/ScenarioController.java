9b8eeaacf82bdc5deeac55f0187f18ecf3f52614
Nikolas <nikolas.serafini@gmail.com>
2015-11-09 14:02:25 -0200

    Started scenarios screen

--- src/main/java/br/ufes/cdsceunes/controller/ScenarioController.java
+++ src/main/java/br/ufes/cdsceunes/controller/ScenarioController.java
@@ -4,10 +4,13 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.ufes.cdsceunes.dao.ScenarioDAO;
+import br.ufes.cdsceunes.model.Scenario;
 
 @Controller
 @Transactional
@@ -24,4 +27,17 @@ public class ScenarioController extends AbstractController{
 		return mad;
 	}
 	
+	@RequestMapping(method = RequestMethod.POST, name="createScenario", value="save")
+	public ModelAndView save() {
+		ModelAndView mad = new ModelAndView();
+		return mad;
+	}
+	
+	@RequestMapping(value ="/show/{id}", method = RequestMethod.GET)
+	public ModelAndView show(@PathVariable(value="id") String id ) {
+		ModelAndView mad = new ModelAndView("scenario/show");
+		Scenario s = scenarios.findById(Long.getLong(id));
+		mad.addObject("scenario", s);
+		return mad;
+	}
 }
--- src/main/java/br/ufes/cdsceunes/model/Scenario.java
+++ src/main/java/br/ufes/cdsceunes/model/Scenario.java
@@ -3,16 +3,11 @@ package br.ufes.cdsceunes.model;
 import java.util.LinkedList;
 import java.util.List;
 
-
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -28,7 +23,11 @@ public class Scenario extends AbstractModel {
 
 	@OneToMany(mappedBy="scenario")
 	private List<DistributionResult> distribution;
+	
+	@Column(nullable=false)
+	boolean activated;
 
+	
 	public void put(DistributionResult result) {
 		distribution.add(result);
 	}
@@ -39,6 +38,11 @@ public class Scenario extends AbstractModel {
 
 	public Scenario() {
 		distribution = new LinkedList<DistributionResult>();
+		activated = false;
+	}
+	
+	public void activate() {
+		activated = true;
 	}
 	
 }
