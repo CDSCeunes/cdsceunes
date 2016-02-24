4cb100aae3907601d2676b59096ef3efcea3af41
Nikolas Serafini <nikolas.serafini@gmail.com>
2016-02-24 17:08:35 -0300

    Algorithm expanded.

--- src/test/java/br/ufes/cdsceunes/lib/distribution/DistributionRunnerTest.java
+++ src/test/java/br/ufes/cdsceunes/lib/distribution/DistributionRunnerTest.java
@@ -27,6 +27,7 @@ public class DistributionRunnerTest {
 	private Discipline architecture;
 	private Discipline prog3;
 	private Discipline os;
+	private Discipline cg;
 
 	@Before
 	public void construct() {
@@ -35,6 +36,7 @@ public class DistributionRunnerTest {
 		architecture = DisciplineBuilder.createDiscipline("Architecture");
 		prog3 = DisciplineBuilder.createDiscipline("Prog3");
 		os = DisciplineBuilder.createDiscipline("OS");
+		cg = DisciplineBuilder.createDiscipline("CG");
 		t1 = TeachersBuilder.createTeacher("T1");
 	}
 
@@ -45,16 +47,19 @@ public class DistributionRunnerTest {
 
 	@Test
 	public void testOneTeacherAndOneDisciplineWithWantPreference() {
+		System.out.println("TEST 1! Pardal should get 1 discipline!");
 		preferences.add(PreferencesBuilder.createPreferences(pardal, architecture, Preference.WANT));
 		List<DistributionResult> results = DistributionRunner.generateDistribution(preferences).getDistributionList();
 		Assert.assertEquals(1, results.size());
 		DistributionResult result = results.get(0);
 		Assert.assertEquals("Pardal", result.getTeacher().getName());
 		Assert.assertEquals("Architecture", result.getDiscipline().getName());
+		DistributionRunner.clearMap();
 	}
 
 	@Test
 	public void testTwoTeacherAndOneDisciplineWithWantAndAcceptPreference() {
+		System.out.println("\nTEST 2! Pardal should get 1 discipline!");
 		preferences.add(PreferencesBuilder.createPreferences(t1, architecture, Preference.ACCEPT));
 		preferences.add(PreferencesBuilder.createPreferences(pardal, architecture, Preference.WANT));
 		List<DistributionResult> results = DistributionRunner.generateDistribution(preferences).getDistributionList();
@@ -62,10 +67,12 @@ public class DistributionRunnerTest {
 		DistributionResult result = results.get(0);
 		Assert.assertEquals("Pardal", result.getTeacher().getName());
 		Assert.assertEquals("Architecture", result.getDiscipline().getName());
+		DistributionRunner.clearMap();
 	}
 
 	@Test
 	public void testMultipleTeachersAndOneDisciplineWithJustAcceptPreferences() {
+		System.out.println("\nTEST 3! Pardal should get 1 discipline!");
 		preferences.add(PreferencesBuilder.createPreferences(t1, architecture, Preference.ACCEPT));
 		preferences.add(PreferencesBuilder.createPreferences(pardal, architecture, Preference.ACCEPT));
 		preferences.add(PreferencesBuilder.createPreferences(TeachersBuilder.createTeacher(), architecture, Preference.ACCEPT));
@@ -74,10 +81,13 @@ public class DistributionRunnerTest {
 		DistributionResult result = results.get(0);
 		Assert.assertEquals("Pardal",result.getTeacher().getName());
 		Assert.assertEquals("Architecture", result.getDiscipline().getName());
+		DistributionRunner.clearMap();
 	}
 	
 	@Test
 	public void testMultipleTeachersWithMultiplePreferences() {
+		DistributionRunner.clearMap();
+		System.out.println("\nTEST 4! Every teacher should get 1 discipline!");
 		preferences.add(PreferencesBuilder.createPreferences(pardal, architecture, Preference.WANT));
 		preferences.add(PreferencesBuilder.createPreferences(pardal, prog3, Preference.NONE));
 		preferences.add(PreferencesBuilder.createPreferences(pardal, os, Preference.ACCEPT));
@@ -106,41 +116,90 @@ public class DistributionRunnerTest {
 		Assert.assertEquals("OS", results.get(1).getDiscipline().getName());	
 		// Pos 2
 		Assert.assertEquals("T2",results.get(2).getTeacher().getName());
-		Assert.assertEquals("Prog3", results.get(2).getDiscipline().getName());	
+		Assert.assertEquals("Prog3", results.get(2).getDiscipline().getName());
+		DistributionRunner.clearMap();
 
 	}
 	
-	/*
-	 * public void testTwoDisciplinesWithWantPreference() { Teacher pardal =
-	 * TeachersBuilder.createTeacher("Pardal"); Teacher joao =
-	 * TeachersBuilder.createTeacher("Joao"); Teacher maria =
-	 * TeachersBuilder.createTeacher("Maria"); Discipline architecture =
-	 * DisciplineBuilder.createDiscipline("Architecture"); Discipline prog3 =
-	 * DisciplineBuilder.createDiscipline("Prog3"); Discipline os =
-	 * DisciplineBuilder.createDiscipline("OS");
-	 * preferences.add(PreferencesBuilder.createPreferences(pardal,
-	 * architecture, Preference.WANT));
-	 * preferences.add(PreferencesBuilder.createPreferences(pardal, os,
-	 * Preference.WANT));
-	 * preferences.add(PreferencesBuilder.createPreferences(pardal, prog3,
-	 * Preference.ACCEPT));
-	 * preferences.add(PreferencesBuilder.createPreferences(maria, prog3,
-	 * Preference.WANT));
-	 * preferences.add(PreferencesBuilder.createPreferences(maria, os,
-	 * Preference.ACCEPT));
-	 * preferences.add(PreferencesBuilder.createPreferences(maria, architecture,
-	 * Preference.NONE));
-	 * preferences.add(PreferencesBuilder.createPreferences(joao, os,
-	 * Preference.WANT));
-	 * preferences.add(PreferencesBuilder.createPreferences(joao, prog3,
-	 * Preference.ACCEPT));
-	 * preferences.add(PreferencesBuilder.createPreferences(joao, architecture,
-	 * Preference.NONE)); List<DistributionResult> results =
-	 * DistributionRunner.generateDistribution(preferences).getDistributionList(
-	 * ); for (DistributionResult dist : results) { String teacher =
-	 * dist.getTeacher().getName();
-	 * 
-	 * } }
-	 */
+	@Test
+	public void testMultipleTeachersWithMultiplePreferencesTeacherGetsMultipleDisciplines() {
+		System.out.println("\nTEST 5! Pardal should get 2 disciplines! And some other guy just 1.");
+		preferences.add(PreferencesBuilder.createPreferences(pardal, architecture, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(pardal, prog3, Preference.NONE));
+		preferences.add(PreferencesBuilder.createPreferences(pardal, os, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t1, architecture, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t1, prog3, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t1, os, Preference.NONE));
+		Teacher t2 = TeachersBuilder.createTeacher("T2");
+		preferences.add(PreferencesBuilder.createPreferences(t2, architecture, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t2, prog3, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t2, os, Preference.NONE));
+		Teacher t3 = TeachersBuilder.createTeacher("T3");
+		preferences.add(PreferencesBuilder.createPreferences(t3, architecture, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t3, prog3, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t3, os, Preference.NONE));
+		Teacher t4 = TeachersBuilder.createTeacher("T4");
+		preferences.add(PreferencesBuilder.createPreferences(t4, architecture, Preference.NONE));
+		preferences.add(PreferencesBuilder.createPreferences(t4, prog3, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t4, os, Preference.ACCEPT));
+		List<DistributionResult> results = DistributionRunner.generateDistribution(preferences).getDistributionList();
+		Assert.assertEquals(3, results.size());
+		// Pos 0
+		Assert.assertEquals("Pardal",results.get(0).getTeacher().getName());
+		Assert.assertEquals("Architecture", results.get(0).getDiscipline().getName());
+		// Pos 1
+		Assert.assertEquals("Pardal",results.get(1).getTeacher().getName());
+		Assert.assertEquals("OS", results.get(1).getDiscipline().getName());	
+		// Pos 2
+		Assert.assertEquals("T2",results.get(2).getTeacher().getName());
+		Assert.assertEquals("Prog3", results.get(2).getDiscipline().getName());	
+		DistributionRunner.clearMap();
+
+	}
+	
+	@Test
+	public void testMultipleTeachersWithMultiplePreferencesWithSomeGuyThatWantsEverything() {
+		
+		System.out.println("\nTEST 6! Pardal should get just 3 disciplines, not 4!");
+		preferences.add(PreferencesBuilder.createPreferences(pardal, architecture, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(pardal, prog3, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(pardal, os, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(pardal, cg, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t1, architecture, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t1, prog3, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t1, os, Preference.NONE));
+		preferences.add(PreferencesBuilder.createPreferences(t1, cg, Preference.WANT));
+		Teacher t2 = TeachersBuilder.createTeacher("T2");
+		preferences.add(PreferencesBuilder.createPreferences(t2, architecture, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t2, prog3, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t2, os, Preference.NONE));
+		preferences.add(PreferencesBuilder.createPreferences(t2, cg, Preference.NONE));
+		Teacher t3 = TeachersBuilder.createTeacher("T3");
+		preferences.add(PreferencesBuilder.createPreferences(t3, architecture, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t3, prog3, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t3, os, Preference.NONE));
+		preferences.add(PreferencesBuilder.createPreferences(t3, cg, Preference.ACCEPT));
+		Teacher t4 = TeachersBuilder.createTeacher("T4");
+		preferences.add(PreferencesBuilder.createPreferences(t4, architecture, Preference.NONE));
+		preferences.add(PreferencesBuilder.createPreferences(t4, prog3, Preference.ACCEPT));
+		preferences.add(PreferencesBuilder.createPreferences(t4, os, Preference.WANT));
+		preferences.add(PreferencesBuilder.createPreferences(t4, cg, Preference.NONE));
+		List<DistributionResult> results = DistributionRunner.generateDistribution(preferences).getDistributionList();
+		Assert.assertEquals(4, results.size());
+		// Pos 0
+		Assert.assertEquals("Pardal",results.get(0).getTeacher().getName());
+		Assert.assertEquals("Architecture", results.get(0).getDiscipline().getName());
+		// Pos 1
+		Assert.assertEquals("Pardal",results.get(1).getTeacher().getName());
+		Assert.assertEquals("CG", results.get(1).getDiscipline().getName());
+		// Pos 2
+		Assert.assertEquals("Pardal",results.get(2).getTeacher().getName());
+		Assert.assertEquals("OS", results.get(2).getDiscipline().getName());	
+		// Pos 3
+		Assert.assertNotEquals("Pardal",results.get(3).getTeacher().getName());
+		Assert.assertEquals("Prog3", results.get(3).getDiscipline().getName());	
+		DistributionRunner.clearMap();
+	}
+	
 
 }
