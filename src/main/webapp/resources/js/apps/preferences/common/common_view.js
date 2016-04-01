ea9325e52ee70abe3eeb2ef81fb1866e3ff8c943
Igor Sant'ana <igorba2005@gmail.com>
2016-04-01 11:29:08 -0300

    Initiating development of forms

---
+++ src/main/webapp/resources/js/apps/preferences/common/common_view.js
@@ -0,0 +1,8 @@
+define(["app"], function(CDSCeunes) {
+	CDSCeunes.module("PreferencesApp.View", View, CDSCeunes, Backbone, Marionette, $, _) {
+		View.Form =  // TODO
+	
+	}
+
+	return CDSCeunes.PreferencesApp.View;
+});
---
+++ src/main/webapp/resources/js/apps/preferences/new/new_view.js
@@ -0,0 +1,5 @@
+define(["app.js"], function("CDSCeunes") {
+	CDSCeunes.module("PreferencesApp.View", View, CDSCeunes, Backbone, Marionette, $, _) {
+		
+	}
+});
