d9a861d2a7ca71ec08e01c59ee7afc7b490edc5d
Nikolas Serafini <nikolas.serafini@gmail.com>
2016-08-31 12:39:02 -0300

    Missing Scenario Controller methods

--- src/main/java/br/ufes/cdsceunes/controller/ScenarioController.java
+++ src/main/java/br/ufes/cdsceunes/controller/ScenarioController.java
@@ -2,6 +2,10 @@ package br.ufes.cdsceunes.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +23,25 @@ public class ScenarioController extends AbstractController {
 	public List<Scenario> listAll() {
 		return repository.findAll();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Scenario getScenarioId(@PathVariable Long id) {
+		return repository.findOne(id);
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<Scenario> save(@RequestBody Scenario scenario) {
+		repository.save(scenario);
+		return new ResponseEntity<Scenario>(scenario, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Scenario> delete(@PathVariable("id") Long id) {
+		Scenario scenario = repository.findOne(id);
+		if(scenario != null) {
+			return new ResponseEntity<Scenario>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<Scenario>(HttpStatus.BAD_REQUEST);
+	}
+	
 }
