72edef08d9dccfab5bc803c3376e5a30673db6aa
Nikolas Serafini <nikolas.serafini@gmail.com>
2016-09-01 12:52:11 -0300

    Something changed

--- src/main/resources/static/js/apps/distributions/new/new_controller.js
+++ src/main/resources/static/js/apps/distributions/new/new_controller.js
@@ -8,7 +8,41 @@ define(["app", "apps/distributions/new/new_view", "q"], function(CDSCeunes, View
           var teachersListView;
           var disciplinesListView;
 
-          
+          Q.all(CDSCeunes.request("discipline:entities", "teacher:entities", "distribution:entities").then(
+            function(distributions) {
+
+              teachersListView = new View.Teachers({
+                collection: teacher,
+              });
+              disciplinesListView = new View.Disciplines({
+                collection: discipline,
+              });
+
+              listPanel.on("distribution", function() {
+                require(["apps/distributions/new/new_view", "entities/distribution", "entities/teacher", "entities/discipline"],
+                  function(NewView) {
+                  var distribution = CDSCeunes.request("distribution:entity:new");
+                  Q.all(CDSCeunes.request("teacher:entities", "discipline:entities")).then(function(teachers, disciplines) {
+                    var newView = new NewView.Distribution({
+                      model: distribution,
+                      teachers: teachers,
+                      disciplines: disciplines
+                    });
+
+                    CDSCeunes.regions.dialog.show(newView);
+
+                    newView.on("distribution:form:submit", function(data) {
+                      if (distribution.save(data)) {
+                        distributions.add(distribution);
+                        newView.trigger("dialog:close");
+                      }
+                    });
+
+                  });
+                });
+              });
+
+          });))
         });
       }
     };
