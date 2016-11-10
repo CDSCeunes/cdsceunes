0dcfbef7baf8b8502fdee639e43f0cefc6e9396e
Igor Sant'ana <igorba2005@gmail.com>
2016-11-10 21:16:00 -0200

    Removing old files

--- src/main/resources/static/js/old/apps/distributions/show/show_controller.js
+++
@@ -1,8 +0,0 @@
-define(["app"], function(CDSCeunes) {
-  CDSCeunes.module("DistributionsApp.ShowController", function(Show, CDSCeunes, Backbone, Marionette, $, _) {
-    Show.Controller = {
-      showController: function(id) {
-      }
-    }
-  });
-});
--- src/main/resources/static/js/old/apps/preferences/common/common_view.js
+++
@@ -1,8 +0,0 @@
-define(["app"], function(CDSCeunes) {
-	CDSCeunes.module("PreferencesApp.View", View, CDSCeunes, Backbone, Marionette, $, _) {
-		View.Form =  // TODO
-	
-	}
-
-	return CDSCeunes.PreferencesApp.View;
-});
